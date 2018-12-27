15c79abc5d57b54dff9af67e7817e9bf068a83d8
Janet Leon <ajleon00@gmail.com>
2018-12-26 17:05:31 -0800

    takes out outline

--- src/App/2- common/navbar.js
+++ src/App/2- common/navbar.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import './navbar.css'
-export default class Navbar extends React.Component{
-    render(){
-        return(
-            <nav data-aos="fade"
-            data-aos-easing="ease-in-out"
-     data-aos-duration="1000" className='navbar'>
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#work">Work</a></li>
-                    <li>Contact</li>
-                    <li>Resume</li>
-                </ul>
-            </nav>
-        )
-    }
+import './navbar.css';
+export default class Navbar extends React.Component {
+	render() {
+		return (
+			<nav data-aos="fade" data-aos-easing="ease-in-out" data-aos-duration="1000" className="navbar">
+				<ul>
+					<li>
+						<a href="#home">Home</a>
+					</li>
+					<li>
+						<a href="#about">About</a>
+					</li>
+					<li>
+						<a href="#work">Work</a>
+					</li>
+					<li>
+						<a href="#contact">Contact</a>
+					</li>
+					<li>
+						<a href="#resume">Resume</a>
+					</li>
+				</ul>
+			</nav>
+		);
+	}
 }
--- src/App/name-intro/name-intro.js
+++ src/App/name-intro/name-intro.js
@@ -11,8 +11,8 @@ export function MyName (){
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#work">Work</a></li>
-                    <li>Contact</li>
-                    <li>Resume</li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#resume">Resume</a></li>
                 </ul>
             </nav>
             <div className='name-intro'>
