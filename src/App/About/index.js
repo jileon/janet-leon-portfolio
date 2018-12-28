d3451526e33b414545e304e98a588577722e184d
Janet Leon <ajleon00@gmail.com>
2018-12-28 15:08:47 -0800

    fixes project button bug and sets up about page

--- src/App/About/index.js
+++ src/App/About/index.js
@@ -1,31 +1,50 @@
 import React from 'react';
-import './about.css'
+import './about.css';
 
 export default function AboutMe(props) {
-    return (
-        <React.Fragment>
-         <div id='about'>
-    
-         </div>
-       
-            <section id='aboutme' className='outline-section about-section'>
-                <section className='outer-rectangle'>
-                    <h3>About</h3>
-                    <section className='inner-rectangle'>
+  return (
+    <React.Fragment>
+      <div id="about" />
 
-                        <section id='aboutMe'>
-
-                            <p className='description'>I’m a web developer and I like to build things on the internet.
-                    I’m passionate about all things web and digital creativity.</p>
-           
-						<a href={require('../Files/JANET_LEON_Resume.pdf')} download>Resume</a>
-			
-                        </section>
-                    </section>
-                </section>
+      <section className="outline-section">
+        <section className="outer-rectangle">
+          <h3>About</h3>
+          <section className="inner-rectangle">
+            <section className="aboutMe">
+              <div className="about-name">
+                <h4>Janet Leon</h4>
+                <br />
+                <p className="bio-paragraph">
+                  <span className="title"> Full Stack Web Developer</span>
+                  <br />
+                  UCLA Alum <br />
+                  Thinkful Graduate <br /> Aerial Hobbyist <br /> Coffee Lover
+                </p>
+              </div>
+              <div className="about-description">
+                <p className="bio-paragraph">
+                  Hi there! I'm Janet and I'm passionate about about taking my
+                  digital toolset to build beautiful web interactions.
+                </p>
+                <br />
+                <p className="bio-tools">
+                  <span className="tl-blt">My Tool Belt</span>
+                  <br />
+                  <strong> Frontend:</strong> JavaScript, React, Redux,jQuery,
+                  HTML5, CSS3 <br />
+                  <br />
+                  <strong>Backend: </strong>Node.js, Express, PostgreSQL,
+                  MongoDB, Mocha & Chai, Passport, REST APIs v3
+                  <br /> <br />
+                  <strong>Other Tools: </strong>
+                  <br /> Git, GitHub, Chrome Dev Tools, Heroku, Travis CI,
+                  pgAdmin4, Robo 3T <br />
+                </p>
+              </div>
             </section>
-        </React.Fragment>
-
-
-    );
-};
+          </section>
+        </section>
+      </section>
+    </React.Fragment>
+  );
+}
