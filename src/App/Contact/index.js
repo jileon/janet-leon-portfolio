4909ab6c5cbb62b7580cbdfb625dd8f5b3fbc4b9
Janet Leon <ajleon00@gmail.com>
2018-12-27 21:38:50 -0800

    sets up buttons

--- src/App/Contact/index.js
+++ src/App/Contact/index.js
@@ -1,61 +1,52 @@
 import React from 'react';
-
 import './contact-ftr.css';
+
 export default function ContactFooter() {
   return (
     <>
       <section id="contact" className="contact-ftr">
-        <section className="contact-inf">
-          <div className="name">
-            <h3>Contact</h3>
-            <h4>ajleon00@gmail.com</h4>
+        <div className="contact">
+          <h2>Intersted in working together?</h2>
+        </div>
+        <div className="connect">
+          <div className="connect-buttons">
+            <button type="button">
+              <a
+                href="https://github.com/jileon/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
+            </button>
+
+            <button type="button">
+              <a
+                href="https://twitter.com/iamjanetleon/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Twitter
+              </a>
+            </button>
+
+            <button type="button">
+              <a
+                href="https://www.linkedin.com/in/janetleon/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </button>
+
+            <button type="button">
+              <a href={require('../Files/JANET_LEON_Resume.pdf')} download>
+                Resume
+              </a>
+            </button>
           </div>
-          <div className="contact">
-            <h3>Connect</h3>
-            <section className="contact-buttons">
-              <div className="grid-item">
-                <button type="button">
-                  <a
-                    href="https://github.com/jileon"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Twitter
-                  </a>
-                </button>
-              </div>
-              <div className="grid-item">
-                <button type="button">
-                  <a
-                    href="https://github.com/jileon"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    LinkedIn
-                  </a>
-                </button>
-              </div>
-              <div className="grid-item">
-                <button type="button">
-                  <a
-                    href="https://github.com/jileon"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Github
-                  </a>
-                </button>
-              </div>
-              <div className="grid-item">
-                <button type="button">
-                  <a href={require('../Files/JANET_LEON_Resume.pdf')} download>
-                    Resume
-                  </a>
-                </button>
-              </div>
-            </section>
-          </div>
-        </section>
+        </div>
       </section>
     </>
   );
