de53e2bdb2d827c58addb9552a5d4a40a09c29cd
Janet Leon <ajleon00@gmail.com>
2018-12-28 17:21:09 -0800

    Updates

---
+++ precache-manifest.2355a9d0aa9197b2cdd37c8ef5c771cb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "aecce5e08a73533eb7e7",
+    "url": "/janet-portfolio/static/css/main.6706320b.chunk.css"
+  },
+  {
+    "revision": "aecce5e08a73533eb7e7",
+    "url": "/janet-portfolio/static/js/main.aecce5e0.chunk.js"
+  },
+  {
+    "revision": "1b3a5f9571ee3fc3a2ae",
+    "url": "/janet-portfolio/static/css/1.4026f026.chunk.css"
+  },
+  {
+    "revision": "1b3a5f9571ee3fc3a2ae",
+    "url": "/janet-portfolio/static/js/1.1b3a5f95.chunk.js"
+  },
+  {
+    "revision": "8b6ce67f1fde1b44d4a2",
+    "url": "/janet-portfolio/static/js/runtime~main.8b6ce67f.js"
+  },
+  {
+    "revision": "fa663b071fa77bde63fe031758413e9b",
+    "url": "/janet-portfolio/static/media/background.fa663b07.png"
+  },
+  {
+    "revision": "3b9243a929bb51521c2373e4739149ea",
+    "url": "/janet-portfolio/static/media/newsflash.3b9243a9.png"
+  },
+  {
+    "revision": "83a9503b393c6d8cba3b929a15791f00",
+    "url": "/janet-portfolio/static/media/spanish-desk.83a9503b.png"
+  },
+  {
+    "revision": "d67e5eae25329a610104b1d4d58fd325",
+    "url": "/janet-portfolio/static/media/knowted.d67e5eae.gif"
+  },
+  {
+    "revision": "d81bea409d340c6310967035632513c8",
+    "url": "/janet-portfolio/static/media/dig.d81bea40.png"
+  },
+  {
+    "revision": "802d117626662f0b48d23c7ee40710a8",
+    "url": "/janet-portfolio/static/media/cbcburrito.802d1176.png"
+  },
+  {
+    "revision": "73f8935ccad20bec650f5dd4733cd4cb",
+    "url": "/janet-portfolio/static/media/JANET_LEON_WEB_DEV_2019_Public.73f8935c.pdf"
+  },
+  {
+    "revision": "f43378a801b865580a16a2a74fa05572",
+    "url": "/janet-portfolio/index.html"
+  }
+];
