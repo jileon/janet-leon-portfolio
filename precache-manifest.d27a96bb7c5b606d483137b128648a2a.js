eb7dcdea3d19ad2b013c4309c1cade4de5dcab37
Janet Leon <ajleon00@gmail.com>
2018-12-28 17:32:12 -0800

    Updates

---
+++ precache-manifest.d27a96bb7c5b606d483137b128648a2a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "79664403a1bc47bf1301",
+    "url": "/static/css/main.89770b7a.chunk.css"
+  },
+  {
+    "revision": "79664403a1bc47bf1301",
+    "url": "/static/js/main.79664403.chunk.js"
+  },
+  {
+    "revision": "1b3a5f9571ee3fc3a2ae",
+    "url": "/static/css/1.4026f026.chunk.css"
+  },
+  {
+    "revision": "1b3a5f9571ee3fc3a2ae",
+    "url": "/static/js/1.1b3a5f95.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa663b071fa77bde63fe031758413e9b",
+    "url": "/static/media/background.fa663b07.png"
+  },
+  {
+    "revision": "3b9243a929bb51521c2373e4739149ea",
+    "url": "/static/media/newsflash.3b9243a9.png"
+  },
+  {
+    "revision": "83a9503b393c6d8cba3b929a15791f00",
+    "url": "/static/media/spanish-desk.83a9503b.png"
+  },
+  {
+    "revision": "d67e5eae25329a610104b1d4d58fd325",
+    "url": "/static/media/knowted.d67e5eae.gif"
+  },
+  {
+    "revision": "d81bea409d340c6310967035632513c8",
+    "url": "/static/media/dig.d81bea40.png"
+  },
+  {
+    "revision": "802d117626662f0b48d23c7ee40710a8",
+    "url": "/static/media/cbcburrito.802d1176.png"
+  },
+  {
+    "revision": "73f8935ccad20bec650f5dd4733cd4cb",
+    "url": "/static/media/JANET_LEON_WEB_DEV_2019_Public.73f8935c.pdf"
+  },
+  {
+    "revision": "b5a3daf5c84aa7fad8776b5314f7ede2",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/janet-portfolio/precache-manifest.2355a9d0aa9197b2cdd37c8ef5c771cb.js"
+  "/precache-manifest.d27a96bb7c5b606d483137b128648a2a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/janet-portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
