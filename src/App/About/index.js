49df32168417187863e86a676437391850e8da8c
Janet Leon <ajleon00@gmail.com>
2019-01-24 14:01:11 -0800

    adds spacing between words

--- src/App/About/index.js
+++ src/App/About/index.js
@@ -69,7 +69,7 @@ export default function AboutMe(props) {
                   </p>
                   <p>
                     <strong> Frontend: </strong> <br />
-                    JavaScript, React, Redux,jQuery, HTML5, CSS3, Sass,
+                    JavaScript, React, Redux, jQuery, HTML5, CSS3, Sass,
                     Bootstrap
                     <br />
                   </p>
